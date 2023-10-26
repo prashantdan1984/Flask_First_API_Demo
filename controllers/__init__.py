63389324fc27d1c15c23a53ed3ef9061aaa3518b
prashantdan1984 <gadhavi.prashantdan@gmail.com>
2023-10-26 13:07:36 +0530

    added new controller in file and updated __init__ file

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,5 +1,5 @@
 import os
 import glob
 
-__all__=["user_controller", "product_controller"]
-#__all__=[os.path.basename(f)[:-3] for f in glob.glob(os.path.dirname(__file__) + "/*.py")]
+#__all__=["user_controller", "product_controller"]
+__all__=[os.path.basename(f)[:-3] for f in glob.glob(os.path.dirname(__file__) + "/*.py")]
---
+++ controllers/product_category_controller.py
@@ -0,0 +1,4 @@
+from app import app
+@app.route("/pcat/addnew")
+def pcat_addnew():
+    return "this is pcate add new screen added."
