5bba7eb9a76cc25b1b5b448be18c3a19ef92d99b
prashantdan1984 <gadhavi.prashantdan@gmail.com>
2023-12-04 17:49:09 +0530

    Curd opration updated

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -1,8 +1,37 @@
 from app import app
 from model.user_model import user_model
+from flask import request
 obj = user_model()
 
 @app.route("/user/signup")
 def signup():
     #return "Welcome to signup screen1"
     return obj.user_signup_model("danbapu");
+
+@app.route("/user/getall")
+def user_getall_controller():
+    #return "Welcome to signup screen1"
+    return obj.user_getall_model("get all model");
+
+@app.route("/user/addone", methods = ['POST'])
+def user_addone_controller():
+    print(request.form)
+    #return "Welcome to signup screen1"
+    return obj.user_addone_model(request.form);
+
+@app.route("/user/update", methods = ['PUT'])
+def user_update_controller():
+    print(request.form)
+    #return "Welcome to signup screen1"
+    return obj.user_update_model(request.form);
+
+@app.route("/user/delete", methods = ['DELETE'])
+def user_delete_controller():
+    print(request.form)
+    #return "Welcome to signup screen1"
+    return obj.user_delete_model(request.form);
+
+@app.route("/user/delete1/<id>", methods = ['DELETE'])
+def user_delete1_controller(id):
+    #return "Welcome to signup screen1"
+    return obj.user_delete_model(id);
--- model/user_model.py
+++ model/user_model.py
@@ -1,6 +1,7 @@
 #import mysql.connector  #then write mysql.connector.connect
 import mysql.connector as mysql # then write mysql.connect
 import json
+
 class user_model():
     def __init__(self):
         try:
@@ -8,6 +9,7 @@ class user_model():
             #connection = mysql.connect(host="localhost", user="root", port='3306', password="12345", database="flask_demo")
             #con=mysql.connecter.connect(user="root", password="12345", host="localhost", database="flask_demo")
             self.con=mysql.connect(user="root", password="12345", host="localhost", database="flask_demo")
+            self.con.autocommit = True
             self.cur=self.con.cursor(dictionary = True)
             print("connection successful")
         except mysql.error as err:
@@ -26,7 +28,70 @@ class user_model():
         result= self.cur.fetchall()
         print(result)
         if len(result) > 0:
-            return json.dumps(result)
+            temp = json.dumps(result)
+            return temp + name #json.dumps(result)
         else:
             return "No Data Found"
-        #return "This is user signup model4" + " " +name
+
+    def user_getall_model(self, name):
+        #business logic
+        #Query execution code. 
+        self.cur.execute("SELECT * FROM users")
+        result= self.cur.fetchall()
+        print(result)
+        if len(result) > 0:
+            temp = json.dumps(result)
+            return temp + name #json.dumps(result)
+        else:
+            return "No Data Found"
+        
+    def user_addone_model(self, userdata):
+        #business logic
+        #Query execution code.
+        #print(userdata['email_id']); 
+        self.cur.execute(f"INSERT INTO users(username, email_id, phone_no, password) VALUES ('{userdata['username']}', '{userdata['email_id']}', '{userdata['phone_no']}', '{userdata['password']}')")
+        return "This is user addone model"
+    
+    def user_update_model(self, userdata):
+        #business logic
+        #Query execution code.
+        self.cur.execute(f"UPDATE users SET username= '{userdata['username']}', email_id= '{userdata['email_id']}', phone_no= '{userdata['phone_no']}', password= '{userdata['password']}' WHERE id= {userdata['id']}")
+        print(userdata['id']);
+        if self.cur.rowcount>0:
+            return "User data update successfully"
+        else:
+            return "nothing to update"
+        
+    def user_delete_model(self, userdata):
+        #business logic
+        #Query execution code.
+        print(userdata);
+        
+        if type(userdata) is str:
+            print("This line will be executed", type(userdata))
+            self.cur.execute(f"DELETE FROM users WHERE id= '{userdata}'")
+            print(userdata['id']);
+            if self.cur.rowcount>0:
+                return "User data deleted successfully"
+            else:
+                return "Nothing to delete"
+        else:
+            print("Not an integer", type(userdata))
+            self.cur.execute(f"DELETE FROM users WHERE id= {userdata['id']}")
+            print(userdata['id']);
+            if self.cur.rowcount>0:
+                return "User data deleted successfully"
+            else:
+                return "Nothing to delete"
+        
+        #return "delete successfully"
+        
+       
+       # self.cur.execute(f"DELETE FROM users WHERE id= {userdata}")
+       # self.cur.execute(f"DELETE FROM users WHERE id= {userdata['id']}")
+       # print(userdata['id']);
+       # if self.cur.rowcount>0:
+       #     return "User data update successfully"
+       # else:
+       #     return "nothing to update"
+    
