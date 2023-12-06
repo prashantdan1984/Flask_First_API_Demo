883be1f3177b11aefda394495e1637a5046a7731
prashantdan1984 <gadhavi.prashantdan@gmail.com>
2023-12-06 09:59:38 +0530

    user model.py updatd with json response

--- model/user_model.py
+++ model/user_model.py
@@ -1,6 +1,7 @@
 #import mysql.connector  #then write mysql.connector.connect
 import mysql.connector as mysql # then write mysql.connect
 import json
+from flask import make_response
 
 class user_model():
     def __init__(self):
@@ -28,10 +29,10 @@ class user_model():
         result= self.cur.fetchall()
         print(result)
         if len(result) > 0:
-            temp = json.dumps(result)
-            return temp + name #json.dumps(result)
+            temp = make_response({"payload":result}, 200) #json.dumps(result)
+            return temp
         else:
-            return "No Data Found"
+            return make_response({"message":"No Data Found"}, 204)
 
     def user_getall_model(self, name):
         #business logic
@@ -40,17 +41,17 @@ class user_model():
         result= self.cur.fetchall()
         print(result)
         if len(result) > 0:
-            temp = json.dumps(result)
-            return temp + name #json.dumps(result)
+            temp = make_response({"payload":result}, 200) #json.dumps(result)
+            return temp
         else:
-            return "No Data Found"
+            return make_response({"message":"No Data Found"}, 204)
         
     def user_addone_model(self, userdata):
         #business logic
         #Query execution code.
         #print(userdata['email_id']); 
         self.cur.execute(f"INSERT INTO users(username, email_id, phone_no, password) VALUES ('{userdata['username']}', '{userdata['email_id']}', '{userdata['phone_no']}', '{userdata['password']}')")
-        return "This is user addone model"
+        return make_response({"message":"This is user addone model"}, 201)
     
     def user_update_model(self, userdata):
         #business logic
@@ -58,40 +59,28 @@ class user_model():
         self.cur.execute(f"UPDATE users SET username= '{userdata['username']}', email_id= '{userdata['email_id']}', phone_no= '{userdata['phone_no']}', password= '{userdata['password']}' WHERE id= {userdata['id']}")
         print(userdata['id']);
         if self.cur.rowcount>0:
-            return "User data update successfully"
+            return make_response({"message":"User data update successfully"}, 201)
         else:
-            return "nothing to update"
+            return make_response({"message":"nothing to update"}, 204)
         
     def user_delete_model(self, userdata):
         #business logic
         #Query execution code.
         print(userdata);
-        
         if type(userdata) is str:
             print("This line will be executed", type(userdata))
             self.cur.execute(f"DELETE FROM users WHERE id= {userdata}")
             print(userdata);
             if self.cur.rowcount>0:
-                return "User data deleted successfully"
+                return make_response({"message":"User data deleted successfully"}, 201)
             else:
-                return "Nothing to delete"
+                return make_response({"message":"Nothing to delete"}, 204)
         else:
             print("Not an integer", type(userdata))
             self.cur.execute(f"DELETE FROM users WHERE id= {userdata['id']}")
             print(userdata['id']);
             if self.cur.rowcount>0:
-                return "User data deleted successfully"
+                return make_response({"message":"User data deleted successfully"}, 201)
             else:
-                return "Nothing to delete"
-        
-        #return "delete successfully"
-        
-       
-       # self.cur.execute(f"DELETE FROM users WHERE id= {userdata}")
-       # self.cur.execute(f"DELETE FROM users WHERE id= {userdata['id']}")
-       # print(userdata['id']);
-       # if self.cur.rowcount>0:
-       #     return "User data update successfully"
-       # else:
-       #     return "nothing to update"
+                return make_response({"message":"Nothing to delete"}, 204)
     
